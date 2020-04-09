4d9167ba893ef3bcc52f6fda0e50570c90d8f228
sammydour <sammydour1@gmail.com>
2020-04-09 11:30:58 +0100

    starting estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,31 +1,33 @@
-def estimator(data): 
+def estimator(data):{
   return data{
 
     data = {
-     'contnent' = 'Africa',
-     'population' = '1.3 billion',
-     'average_age' = '19.7',
-     'time_elapse' = '60 days',
-     'infected_countries' = '52',
-     'virus_free_countries' = '2',
+      'contnent' = 'Africa',
+      'population' = '1.3 billion',
+      'average_age' = '19.7',
+      'time_elapse' = '60 days',
+      'infected_countries' = '52',
+      'virus_free_countries' = '2',
     }
     impact = {
-    "reported_casses" :"150000",
-    "confirmed_casses" = "107000",
-    "currently_infected" = "100000",
-    "total_hospital_beds" = "15309",
-    "total_no_of_death" ="534",
-    "recorveries" = "1192",
-  }
+      "reported_casses" :"150000",
+      "confirmed_casses" = "107000",
+      "currently_infected" = "100000",
+      "total_hospital_beds" = "15309",
+      "total_no_of_death" ="534",
+      "recorveries" = "1192",
+   }
 
     severe_impact = {
-     'shortage_of_hospital'= '18000',
-     'shortage_of_hospital_beds' = '139300',
-     'shortage_of_ventilators' = '95000',
-     'hike_in_food_price' = '300%',
-     'icu_casses_at_requested_time' = '-24000',        
-    };
-  }
+      'shortage_of_hospital'= '18000',
+      'shortage_of_hospital_beds' = '139300',
+      'shortage_of_ventilators' = '95000',
+      'hike_in_food_price' = '300%',
+      'icu_casses_at_requested_time' = '-24000',        
+    }
+  };
+};
+export default covid19ImpactEstimator;
    
 
 
